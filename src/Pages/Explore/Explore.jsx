17d0d982bec73ab632436bb2606d390563b0407d
Vishal Singh Rawat <56162449+vishalsinghrawat29@users.noreply.github.com>
2023-06-21 18:11:34 +0530

    feat: show posts on explore page

--- src/Pages/Explore/Explore.jsx
+++ src/Pages/Explore/Explore.jsx
@@ -1,8 +1,66 @@
+import { useContext } from "react";
+import { DataContext } from "../../Contexts/DataContext";
+import { useState } from "react";
+import { useEffect } from "react";
+import { Sidebar } from "../../Components/Sidebar/Sidebar";
+import { PostCard } from "../../Components/PostCard/PostCard";
+import { SuggestedUsers } from "../../Components/SuggestedUsers/SuggestedUsers";
+import { FilterPosts } from "../../Utils/FilterPosts";
+
 const Explore = () => {
+  const {
+    dataState: { posts },
+    isPostsLoading,
+  } = useContext(DataContext);
+
+  const [showOptions, setShowOptions] = useState(null);
+
+  const handleShowOptions = (postId) => {
+    setShowOptions((prev) => (prev === postId ? null : postId));
+  };
+
+  const handleClickOutside = () => {
+    setShowOptions(null);
+  };
+
+  useEffect(() => {
+    document.addEventListener("click", handleClickOutside);
+
+    return () => {
+      document.removeEventListener("click", handleClickOutside);
+    };
+  }, []);
+  const latestPosts = FilterPosts(posts, "Latest");
+
   return (
-    <>
-      <h1>Explore Page</h1>
-    </>
+    <div className="home-page-container">
+      <div className="sidebar-box">
+        <Sidebar />
+      </div>
+      <div className="body-box">
+        <h1>Explore</h1>
+
+        <div className="post-cards-box">
+          {isPostsLoading ? (
+            <p>Loading...</p>
+          ) : latestPosts?.length ? (
+            latestPosts.map((post) => (
+              <PostCard
+                post={post}
+                key={post._id}
+                showOptions={showOptions}
+                handleShowOptions={handleShowOptions}
+              />
+            ))
+          ) : (
+            <p>No Posts</p>
+          )}
+        </div>
+      </div>
+      <div className="profile-box">
+        <SuggestedUsers />
+      </div>
+    </div>
   );
 };
 export { Explore };
--- src/Utils/FilterPosts.jsx
+++ src/Utils/FilterPosts.jsx
@@ -1,18 +1,18 @@
 const FilterPosts = (posts, filterBy) => {
   if (filterBy === "Oldest") {
-    return [...posts].sort(
+    return [...posts]?.sort(
       (a, b) => new Date(a.createdAt) - new Date(b.createdAt)
     );
   }
 
   if (filterBy === "Latest") {
-    return [...posts].sort(
+    return [...posts]?.sort(
       (a, b) => new Date(b.createdAt) - new Date(a.createdAt)
     );
   }
 
   if (filterBy === "Trending") {
-    return [...posts].sort((a, b) => b.likes.likeCount - a.likes.likeCount);
+    return [...posts]?.sort((a, b) => b.likes.likeCount - a.likes.likeCount);
   }
 
   return posts;
