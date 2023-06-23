82a5976a5498b513316d0d4caf5b06c529eea625
Vishal Singh Rawat <56162449+vishalsinghrawat29@users.noreply.github.com>
2023-06-23 18:13:44 +0530

    feat: add Welcome Page

--- src/App.js
+++ src/App.js
@@ -1,11 +1,9 @@
 import "./App.css";
-import { Navbar } from "./Components/Navbar/Navbar";
 import { AppRoutes } from "./Routes/AppRoutes";
 
 function App() {
   return (
     <div className="App">
-      <Navbar />
       <AppRoutes />
     </div>
   );
--- src/Pages/Welcome/Welcome.jsx
+++ src/Pages/Welcome/Welcome.jsx
@@ -1,8 +1,33 @@
+import { useNavigate } from "react-router-dom";
+import WelcomeBackground from "../../Assets/Welcome-background.jpg";
+import logo from "../../Assets/iShare-Logo.png";
+import "./WelcomeStyle.css";
 const Welcome = () => {
+  const navigate = useNavigate();
+
   return (
-    <>
-      <h1>Welcome Page</h1>
-    </>
+    <div className="welcome-container">
+      <img src={WelcomeBackground} alt="background" className="welcome-img" />
+      <div className="welcome-body">
+        <p className="logo-container">
+          <img src={logo} alt="iShare" className="logo" /> <span>iShare</span>
+        </p>
+
+        <h1>Join, Connect, Share!</h1>
+        <p>
+          Welcome to iShare platform! Join our vibrant community today and
+          connect with friends from around the world. Share your thoughts,
+          photos, and experiences with ease. Sign up now and let your voice be
+          heard.
+        </p>
+        <button className="primary-btn" onClick={() => navigate("./signup")}>
+          Join Now
+        </button>
+        <button className="secondary-btn" onClick={() => navigate("./login")}>
+          Alread Have Account !
+        </button>
+      </div>
+    </div>
   );
 };
 export { Welcome };
