5f43777e3348dbf81fef6c996f71902e16b78f65
Vishal Singh Rawat <56162449+vishalsinghrawat29@users.noreply.github.com>
2023-06-25 21:06:06 +0530

    refactor: welcome page style

--- src/Pages/Welcome/Welcome.jsx
+++ src/Pages/Welcome/Welcome.jsx
@@ -1,32 +1,32 @@
 import { useNavigate } from "react-router-dom";
-import WelcomeBackground from "../../Assets/Welcome-background.jpg";
-import logo from "../../Assets/iShare-Logo.png";
+import WelcomeBackground from "../../Assets/WelcomeImg.jpg";
+import logo from "../../Assets/iShareLogo.png";
 import "./WelcomeStyle.css";
 const Welcome = () => {
   const navigate = useNavigate();
 
   return (
     <div className="welcome-container">
-      <img src={WelcomeBackground} alt="background" className="welcome-img" />
       <div className="welcome-body">
-        <p className="logo-container">
-          <img src={logo} alt="iShare" className="logo" /> <span>iShare</span>
-        </p>
+        <div className="logo-container">
+          <img src={logo} alt="iShare" />
+          <span>iShare</span>
+        </div>
 
-        <h1>Join, Connect, Share!</h1>
+        <h1>Connect. Explore. Share with iShare.</h1>
         <p>
-          Welcome to iShare platform! Join our vibrant community today and
-          connect with friends from around the world. Share your thoughts,
-          photos, and experiences with ease. Sign up now and let your voice be
-          heard.
+          Welcome to iShare, the social media app that empowers you to connect,
+          discover, and share seamlessly. With iShare, you can connect with
+          friends, explore new horizons, and express yourself effortlessly.
         </p>
         <button className="primary-btn" onClick={() => navigate("./signup")}>
           Join Now
         </button>
-        <button className="secondary-btn" onClick={() => navigate("./login")}>
-          Alread Have Account !
+        <button className="link-btn" onClick={() => navigate("./login")}>
+          Already have an account?
         </button>
       </div>
+      <img src={WelcomeBackground} alt="background" className="welcome-img" />
     </div>
   );
 };
