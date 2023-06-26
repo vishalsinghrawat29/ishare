6f3f4c37679683aecc08624203f5653756349b7d
Vishal Singh Rawat <56162449+vishalsinghrawat29@users.noreply.github.com>
2023-06-26 13:39:15 +0530

    Style: login page

--- src/Pages/Login/Login.jsx
+++ src/Pages/Login/Login.jsx
@@ -1,7 +1,10 @@
 import { useContext, useState } from "react";
 import { FaEye, FaEyeSlash } from "react-icons/fa";
 import { AuthContext } from "../../index";
-import { NavLink } from "react-router-dom";
+import { useNavigate } from "react-router-dom";
+import loginImg from "../../Assets/iShareLogin.jpg";
+import "./LoginStyle.css";
+
 const Login = () => {
   const { loginUser } = useContext(AuthContext);
   const [loginData, setLoginData] = useState({ username: "", password: "" });
@@ -22,79 +25,58 @@ const Login = () => {
     loginUser(guestLoginUser);
   };
 
+  const navigate = useNavigate();
+
   return (
-    <div
-      style={{
-        maxWidth: "300px",
-        margin: "1rem auto",
-        backgroundColor: "antiquewhite",
-      }}
-    >
-      <h1 style={{ padding: "1rem 0" }}>Login Page</h1>
-      <form
-        onSubmit={(e) => loginHandler(e)}
-        style={{
-          display: "flex",
-          flexDirection: "column",
-          alignItems: "center",
-          justifyContent: "center",
-          gap: "1rem",
-        }}
-      >
-        <div
-          style={{
-            display: "flex",
-            flexDirection: "column",
-            alignItems: "center",
-            justifyContent: "center",
-            gap: "0.5rem",
-          }}
-        >
-          <label htmlFor="userName">
-            Username <span>*</span>
-          </label>
-          <input
-            type="text"
-            id="userName"
-            value={loginData?.username}
-            onChange={(e) =>
-              setLoginData((prev) => ({ ...prev, username: e.target.value }))
-            }
-            required
-          />
+    <div className="login">
+      <div className="login-img">
+        <img src={loginImg} alt="ishare-login" />
+      </div>
+      <div className="login-container">
+        <h1>Login</h1>
+        <form onSubmit={(e) => loginHandler(e)} className="login-form">
+          <div className="login-field">
+            <label htmlFor="userName">
+              Username <span>*</span>
+            </label>
+            <input
+              type="text"
+              id="userName"
+              value={loginData?.username}
+              onChange={(e) =>
+                setLoginData((prev) => ({ ...prev, username: e.target.value }))
+              }
+              required
+            />
+          </div>
+          <div className="login-field">
+            <label htmlFor="password">
+              Password <span>*</span>
+            </label>
+            <input
+              type={hidePwd ? "password" : "text"}
+              id="password"
+              value={loginData?.password}
+              onChange={(e) =>
+                setLoginData((prev) => ({ ...prev, password: e.target.value }))
+              }
+              required
+            />
+            <span onClick={() => setHidePwd(!hidePwd)}>
+              {hidePwd ? <FaEye /> : <FaEyeSlash />}
+            </span>
+          </div>
+          <div className="login-btn">
+            <button type="submit">Login</button>
+            <button onClick={guestLoginHandler}>Guest Login</button>
+          </div>
+        </form>
+        <div className="login-link">
+          <p>
+            Don't have an account?{" "}
+            <span onClick={() => navigate("/signup")}>SignUp</span>
+          </p>
         </div>
-        <div
-          style={{
-            display: "flex",
-            flexDirection: "column",
-            alignItems: "center",
-            justifyContent: "center",
-            gap: "0.5rem",
-          }}
-        >
-          <label htmlFor="password">
-            Password <span>*</span>
-          </label>
-          <input
-            type={hidePwd ? "password" : "text"}
-            id="password"
-            value={loginData?.password}
-            onChange={(e) =>
-              setLoginData((prev) => ({ ...prev, password: e.target.value }))
-            }
-            required
-          />
-          <span onClick={() => setHidePwd(!hidePwd)}>
-            {hidePwd ? <FaEye /> : <FaEyeSlash />}
-          </span>
-        </div>
-        <div>
-          <button type="submit">Login</button>
-          <button onClick={guestLoginHandler}>Guest Login</button>
-        </div>
-      </form>
-      <div>
-        <p>Don't have an account? </p> <NavLink to="/signup">SignUp</NavLink>{" "}
       </div>
     </div>
   );
